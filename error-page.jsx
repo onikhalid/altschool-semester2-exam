a3fec69fa40fc515278ee601458cca99bc152463
Oni Khalid Ayòbámi <onikhalidayo@gmail.com>
2024-04-22 10:08:53 +0100

    add 404 not found page

--- error-page.jsx
+++ error-page.jsx
@@ -1,3 +1,5 @@
+import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
@@ -5,12 +7,28 @@ export default function ErrorPage() {
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+    <div id="error-page" >
+      <div className="flex flex-col items-center justify-center w-screen h-screen bg-foreground text-background">
+        <FontAwesomeIcon icon={faTriangleExclamation} size='10x' />
+
+        <h1 className="mt-8" style={{ fontSize: "4rem" }}>
+          {error.status === 404 ? "404" : "Oops!"}
+        </h1>
+        <p className="text-center" style={{ maxWidth: "25ch" }}>
+          {
+            error.status === 404 ?
+              "The page you are looking for could not be found."
+              :
+              "Sorry, an unexpected error has occurred."
+          }
+        </p>
+
+        {error.status !== 404 &&
+          <p className="text-sm">
+            <i>{error.statusText || error.message}</i>
+          </p>
+        }
+      </div>
     </div>
   );
 }
